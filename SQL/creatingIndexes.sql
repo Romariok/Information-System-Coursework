4139b906c64b7a3594c3926c77b96823ef5eff8c
ta4ilka69 <artem_balin_2004@mail.ru>
2024-11-07 16:46:21 +0300

    perfomance test, indexes added

---
+++ SQL/creatingIndexes.sql
@@ -0,0 +1,13 @@
+CREATE INDEX idx_user_musician_subscription_user ON user_musician_subscription (user_id);
+CREATE INDEX idx_user_musician_subscription_musician ON user_musician_subscription (musician_id);
+
+CREATE INDEX idx_product_price_rate_brand ON product (price, rate, brand_id);
+
+CREATE INDEX idx_product_brand ON product (brand_id);
+CREATE INDEX idx_product_guitar_form ON product (guitar_form_id);
+CREATE INDEX idx_product_type ON product (type_of_product_id);
+
+
+CREATE INDEX idx_user_musician_musician_user ON user_musician_subscription (musician_id, user_id);
+CREATE INDEX idx_product_genre_genre_id ON product_genre (genre_id);
+CREATE INDEX idx_product_brand_price_rate ON product (brand_id, price, rate);
