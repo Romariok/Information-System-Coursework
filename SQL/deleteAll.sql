131521077666ed916703143074821c13545ed767
Romariok <ronya-ko@yandex.ru>
2024-11-14 14:54:20 +0300

    new scripts

--- SQL/deleteAll.sql
+++ SQL/deleteAll.sql
@@ -16,6 +16,12 @@ DROP TABLE IF EXISTS musician_genre CASCADE;
 
 DROP TABLE IF EXISTS user_musician_subscription CASCADE;
 
+DROP TABLE IF EXISTS custom_product_collection_product CASCADE;
+
+DROP TABLE IF EXISTS shop_product CASCADE;
+
+DROP TABLE IF EXISTS shop CASCADE;
+
 DROP TABLE IF EXISTS feedback CASCADE;
 
 DROP TABLE IF EXISTS articles CASCADE;
@@ -26,12 +32,32 @@ DROP TABLE IF EXISTS app_user CASCADE;
 
 DROP TABLE IF EXISTS musician CASCADE;
 
-DROP TABLE IF EXISTS type_of_product CASCADE;
-
-DROP TABLE IF EXISTS guitar_form CASCADE;
-
 DROP TABLE IF EXISTS brand CASCADE;
 
-DROP TABLE IF EXISTS genre CASCADE;
+DROP TABLE IF EXISTS forum_post CASCADE;
 
-DROP TABLE IF EXISTS type_of_musician CASCADE;
+DROP TABLE IF EXISTS forum_topic CASCADE;
+
+-- DROP TABLE IF EXISTS custom_product_collection CASCADE;
+
+-- DROP TABLE IF EXISTS custom_product_collection_product CASCADE;
+
+DROP TYPE IF EXISTS color_enum;
+
+DROP TYPE IF EXISTS type_of_product_enum;
+
+DROP TYPE IF EXISTS tip_material_enum;
+
+DROP TYPE IF EXISTS body_material_enum;
+
+DROP TYPE IF EXISTS pickup_configuration_enum;
+
+DROP TYPE IF EXISTS type_combo_amplifier_enum;
+
+DROP TYPE IF EXISTS guitar_form_enum;
+
+DROP TYPE IF EXISTS country_enum;
+
+DROP TYPE IF EXISTS type_of_musician_enum;
+
+DROP TYPE IF EXISTS genre_enum;
