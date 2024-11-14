2b1c0731bc5c928557f3b3472c77aa591eaa7ce3
Romariok <ronya-ko@yandex.ru>
2024-11-14 14:54:20 +0300

    new scripts

--- SQL/initScript.sql
+++ SQL/initScript.sql
@@ -1,88 +1,239 @@
-CREATE TABLE type_of_musician (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
+CREATE TYPE color_enum AS ENUM(
+    'BLACK',
+    'WHITE',
+    'RED',
+    'BLUE',
+    'GREEN',
+    'YELLOW',
+    'PURPLE',
+    'ORANGE',
+    'GREY',
+    'BROWN'
 );
 
-CREATE TABLE genre (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
+CREATE TYPE type_of_product_enum AS ENUM(
+    'PEDALS_AND_EFFECTS',
+    'ELECTRIC_GUITAR',
+    'STUDIO_RECORDING_GEAR',
+    'KEYS_AND_MIDI',
+    'AMPLIFIER',
+    'DRUMMS_AND_PERCUSSION',
+    'BASS_GUITAR',
+    'ACOUSTIC_GUITAR',
+    'SOFTWARE_AND_ACCESSORIES'
+);
+
+CREATE TYPE tip_material_enum AS ENUM(
+    'WOOD',
+    'METAL',
+    'PLASTIC',
+    'COMPOSITE'
+);
+
+CREATE TYPE body_material_enum AS ENUM(
+    'MAHOGANY',
+    'MAPLE',
+    'ASH',
+    'ALDER',
+    'BASSWOOD'
+);
+
+CREATE TYPE pickup_configuration_enum AS ENUM(
+    'SSS',
+    'HSS',
+    'HH',
+    'P90',
+    'SINGLE'
+);
+
+CREATE TYPE type_combo_amplifier_enum AS ENUM(
+    'TUBE',
+    'SOLID STATE',
+    'HYBRID',
+    'MODELING'
+);
+
+CREATE TYPE guitar_form_enum AS ENUM(
+    'STRATOCASTER',
+    'TELECASTER',
+    'THINLINE',
+    'LES PAUL',
+    'V',
+    'JAGUAR',
+    'JAZZ-MASTER',
+    'MOCKINGBIRD',
+    'STAR',
+    'MUSTANG'
+);
+
+CREATE TYPE country_enum AS ENUM(
+    'USA',
+    'JAPAN',
+    'GERMANY',
+    'UNITED KINGDOM',
+    'CHINA',
+    'SOUTH KOREA',
+    'INDONESIA',
+    'MEXICO',
+    'CANADA',
+    'ITALY',
+    'FRANCE',
+    'BRAZIL',
+    'AUSTRALIA',
+    'RUSSIA'
+);
+
+CREATE TYPE type_of_musician_enum AS ENUM(
+    'MUSICAL PRODUCER',
+    'GUITARIST',
+    'DRUMMER',
+    'BASSIST',
+    'SINGER',
+    'RAPPER',
+    'KEYBOARDIST'
+);
+
+CREATE TYPE genre_enum AS ENUM(
+    'BLUES',
+    'ROCK',
+    'POP',
+    'JAZZ',
+    'RAP',
+    'METAL',
+    'CLASSICAL',
+    'REGGAE',
+    'ELECTRONIC',
+    'HIP-HOP'
 );
 
 CREATE TABLE brand (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE guitar_form (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE type_of_product (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
+    name TEXT NOT NULL,
+    country country_enum NOT NULL,
+    website TEXT,
+    email TEXT
 );
 
 CREATE TABLE musician (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    subscribers INTEGER NOT NULL
+    name TEXT NOT NULL,
+    subscribers INTEGER NOT NULL CHECK (subscribers >= 0) DEFAULT 0
 );
 
 CREATE TABLE app_user (
     id SERIAL PRIMARY KEY,
     is_admin BOOLEAN DEFAULT FALSE,
-    login VARCHAR(100) UNIQUE NOT NULL,
+    login TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
-    subscriptions INTEGER
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    subscriptions INTEGER CHECK (subscriptions >= 0) DEFAULT 0
 );
 
 CREATE TABLE product (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    name TEXT NOT NULL,
     description TEXT,
-    price NUMERIC(10, 2) NOT NULL,
-    rate NUMERIC(2, 1),
+    rate NUMERIC(2, 1) CHECK (
+        rate >= 0
+        AND rate <= 5
+    ) DEFAULT 0,
     brand_id INTEGER,
-    guitar_form_id INTEGER,
-    type_of_product_id INTEGER,
+    guitar_form guitar_form_enum,
+    type_of_product type_of_product_enum NOT NULL,
     lads INTEGER,
-    color VARCHAR(100) NOT NULL,
+    avg_price NUMERIC(10, 2) CHECK (avg_price >= 0) DEFAULT 0,
+    color color_enum NOT NULL,
     strings INTEGER,
-    tip_material VARCHAR(100),
-    body_material VARCHAR(100),
-    pickup_configuration VARCHAR(100),
-    type_combo_amplifier VARCHAR(100),
-    CONSTRAINT fk_product_brand FOREIGN KEY (brand_id) REFERENCES brand (id) ON DELETE SET NULL,
-    CONSTRAINT fk_product_guitar_form FOREIGN KEY (guitar_form_id) REFERENCES guitar_form (id) ON DELETE SET NULL,
-    CONSTRAINT fk_product_type_of_product FOREIGN KEY (type_of_product_id) REFERENCES type_of_product (id) ON DELETE SET NULL
+    tip_material tip_material_enum,
+    body_material body_material_enum,
+    pickup_configuration pickup_configuration_enum,
+    type_combo_amplifier type_combo_amplifier_enum,
+    CONSTRAINT fk_product_brand FOREIGN KEY (brand_id) REFERENCES brand (id) ON DELETE SET NULL
 );
 
 CREATE TABLE articles (
     id SERIAL PRIMARY KEY,
-    header VARCHAR(200) NOT NULL,
+    header TEXT NOT NULL,
     text TEXT NOT NULL,
-    author INTEGER,
-    date DATE NOT NULL,
-    tags VARCHAR(200),
+    author_id INTEGER,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     accepted BOOLEAN DEFAULT FALSE,
-    CONSTRAINT fk_articles_user FOREIGN KEY (author) REFERENCES app_user (id) ON DELETE SET NULL
+    CONSTRAINT fk_articles_user FOREIGN KEY (author_id) REFERENCES app_user (id) ON DELETE SET NULL
 );
 
 CREATE TABLE feedback (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER,
+    author_id INTEGER NOT NULL,
     product_id INTEGER,
     article_id INTEGER,
-    text TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    text TEXT NOT NULL,
     stars INTEGER CHECK (
-        stars >= 1
+        stars >= 0
         AND stars <= 5
     ),
-    CONSTRAINT fk_feedback_user FOREIGN KEY (user_id) REFERENCES app_user (id) ON DELETE CASCADE,
+    CONSTRAINT fk_feedback_user FOREIGN KEY (author_id) REFERENCES app_user (id) ON DELETE CASCADE,
     CONSTRAINT fk_feedback_product FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE,
     CONSTRAINT fk_feedback_article FOREIGN KEY (article_id) REFERENCES articles (id) ON DELETE CASCADE
 );
+
+CREATE TABLE forum_topic (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    author_id INTEGER,
+    is_closed BOOLEAN DEFAULT FALSE,
+    CONSTRAINT fk_forum_topic_user FOREIGN KEY (author_id) REFERENCES app_user (id) ON DELETE SET NULL
+);
+
+CREATE TABLE forum_post (
+    id SERIAL PRIMARY KEY,
+    topic_id INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    author_id INTEGER,
+    CONSTRAINT fk_forum_post_topic FOREIGN KEY (topic_id) REFERENCES forum_topic (id) ON DELETE CASCADE,
+    CONSTRAINT fk_forum_post_user FOREIGN KEY (author_id) REFERENCES app_user (id) ON DELETE SET NULL
+);
+
+
+CREATE TABLE shop (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT,
+    website TEXT,
+    email TEXT,
+    address TEXT
+);
+
+CREATE TABLE shop_product (
+    shop_id INTEGER,
+    product_id INTEGER,
+    price NUMERIC(10, 2) NOT NULL CHECK (price >= 0),
+    available BOOLEAN DEFAULT TRUE,
+    CONSTRAINT pk_shop_product PRIMARY KEY (shop_id, product_id),
+    CONSTRAINT fk_shop_product_shop FOREIGN KEY (shop_id) REFERENCES shop (id) ON DELETE CASCADE,
+    CONSTRAINT fk_shop_product_product FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE
+);
+
+-- CREATE TABLE custom_product_collection (
+--     id SERIAL PRIMARY KEY,
+--     name TEXT NOT NULL,
+--     description TEXT,
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--     author_id INTEGER,
+--     CONSTRAINT fk_custom_product_collection_user FOREIGN KEY (author_id) REFERENCES app_user (id) ON DELETE SET NULL
+-- );
+
+-- CREATE TABLE custom_product_collection_product (
+--     collection_id INTEGER,
+--     product_id INTEGER,
+--     CONSTRAINT pk_custom_product_collection_product PRIMARY KEY (collection_id, product_id),
+--     CONSTRAINT fk_custom_product_collection_product_collection FOREIGN KEY (collection_id) REFERENCES custom_product_collection (id) ON DELETE CASCADE,
+--     CONSTRAINT fk_custom_product_collection_product_product FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE
+-- );
+
 
 CREATE TABLE user_musician_subscription (
     user_id INTEGER,
@@ -94,44 +245,36 @@ CREATE TABLE user_musician_subscription (
 
 CREATE TABLE musician_genre (
     musician_id INTEGER,
-    genre_id INTEGER,
-    CONSTRAINT pk_musician_genre PRIMARY KEY (musician_id, genre_id),
-    CONSTRAINT fk_musician_genre_musician FOREIGN KEY (musician_id) REFERENCES musician (id) ON DELETE CASCADE,
-    CONSTRAINT fk_musician_genre_genre FOREIGN KEY (genre_id) REFERENCES genre (id) ON DELETE CASCADE
+    genre genre_enum,
+    CONSTRAINT pk_musician_genre PRIMARY KEY (musician_id, genre),
+    CONSTRAINT fk_musician_genre_musician FOREIGN KEY (musician_id) REFERENCES musician (id) ON DELETE CASCADE
 );
 
 CREATE TABLE product_genre (
     product_id INTEGER,
-    genre_id INTEGER,
-    CONSTRAINT pk_product_genre PRIMARY KEY (product_id, genre_id),
-    CONSTRAINT fk_product_genre_product FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE,
-    CONSTRAINT fk_product_genre_genre FOREIGN KEY (genre_id) REFERENCES genre (id) ON DELETE CASCADE
+    genre genre_enum,
+    CONSTRAINT pk_product_genre PRIMARY KEY (product_id, genre),
+    CONSTRAINT fk_product_genre_product FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE
 );
 
 CREATE TABLE type_of_musician_user (
-    type_of_musician_id INTEGER,
+    type_of_musician type_of_musician_enum,
     user_id INTEGER,
-    CONSTRAINT pk_type_of_musician_user PRIMARY KEY (type_of_musician_id, user_id),
-    CONSTRAINT fk_type_of_musician_user_type_of_musician FOREIGN KEY (type_of_musician_id) REFERENCES type_of_musician (id) ON DELETE CASCADE,
+    CONSTRAINT pk_type_of_musician_user PRIMARY KEY (type_of_musician, user_id),
     CONSTRAINT fk_type_of_musician_user_user FOREIGN KEY (user_id) REFERENCES app_user (id) ON DELETE CASCADE
 );
 
 CREATE TABLE type_of_musician_musician (
-    type_of_musician_id INTEGER,
+    type_of_musician type_of_musician_enum,
     musician_id INTEGER,
-    CONSTRAINT pk_type_of_musician_musician PRIMARY KEY (
-        type_of_musician_id,
-        musician_id
-    ),
-    CONSTRAINT fk_type_of_musician_musician_type_of_musician FOREIGN KEY (type_of_musician_id) REFERENCES type_of_musician (id) ON DELETE CASCADE,
+    CONSTRAINT pk_type_of_musician_musician PRIMARY KEY (type_of_musician, musician_id),
     CONSTRAINT fk_type_of_musician_musician_musician FOREIGN KEY (musician_id) REFERENCES musician (id) ON DELETE CASCADE
 );
 
 CREATE TABLE genre_user (
-    genre_id INTEGER,
+    genre genre_enum,
     user_id INTEGER,
-    CONSTRAINT pk_genre_user PRIMARY KEY (genre_id, user_id),
-    CONSTRAINT fk_genre_user_genre FOREIGN KEY (genre_id) REFERENCES genre (id) ON DELETE CASCADE,
+    CONSTRAINT pk_genre_user PRIMARY KEY (genre, user_id),
     CONSTRAINT fk_genre_user_user FOREIGN KEY (user_id) REFERENCES app_user (id) ON DELETE CASCADE
 );
 
@@ -158,6 +301,37 @@ CREATE TABLE product_articles (
     CONSTRAINT fk_product_articles_product FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE,
     CONSTRAINT fk_product_articles_article FOREIGN KEY (article_id) REFERENCES articles (id) ON DELETE CASCADE
 );
+
+CREATE OR REPLACE FUNCTION update_product_average_price()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF TG_OP = 'DELETE' THEN
+        UPDATE product
+        SET avg_price = (
+            SELECT AVG(price)::numeric(10,2)
+            FROM shop_product
+            WHERE product_id = OLD.product_id AND available = true
+        )
+        WHERE id = OLD.product_id;
+    ELSE
+        UPDATE product
+        SET avg_price = (
+            SELECT AVG(price)::numeric(10,2)
+            FROM shop_product
+            WHERE product_id = NEW.product_id AND available = true
+        )
+        WHERE id = NEW.product_id;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_update_product_average_price AFTER
+INSERT
+    OR
+UPDATE
+OR DELETE ON shop_product FOR EACH ROW
+EXECUTE FUNCTION update_product_average_price ();
 
 CREATE OR REPLACE FUNCTION update_product_rating()
 RETURNS TRIGGER AS $$
@@ -238,63 +412,3 @@ INSERT
     OR DELETE ON user_musician_subscription FOR EACH ROW
 EXECUTE FUNCTION update_musician_subscribers ();
 
-INSERT INTO
-    type_of_musician (name)
-VALUES ('MUSICAL PRODUCER'),
-    ('GUITARIST'),
-    ('DRUMMER'),
-    ('BASSIST'),
-    ('SINGER'),
-    ('RAPPER'),
-    ('KEYBOARDIST');
-
-INSERT INTO
-    genre (name)
-VALUES ('BLUES'),
-    ('ROCK'),
-    ('POP'),
-    ('JAZZ'),
-    ('RAP'),
-    ('METAL'),
-    ('CLASSICAL'),
-    ('REGGAE'),
-    ('ELECTRONIC'),
-    ('HIP-HOP');
-
-INSERT INTO
-    brand (name)
-VALUES ('FENDER'),
-    ('GIBSON'),
-    ('YAMAHA'),
-    ('IBANEZ'),
-    ('ESP'),
-    ('PRS'),
-    ('SQUIER'),
-    ('MARTIN'),
-    ('COLLINGS'),
-    ('TAYLOR');
-
-INSERT INTO
-    guitar_form (name)
-VALUES ('STRATOCASTER'),
-    ('TELECASTER'),
-    ('THINLINE'),
-    ('LES PAUL'),
-    ('V'),
-    ('JAGUAR'),
-    ('JAZZ-MASTER'),
-    ('MOCKINGBIRD'),
-    ('STAR'),
-    ('MUSTANG');
-
-INSERT INTO
-    type_of_product (name)
-VALUES ('PEDALS_AND_EFFECTS'),
-    ('ELECTRIC_GUITAR'),
-    ('STUDIO_RECORDING_GEAR'),
-    ('KEYS_AND_MIDI'),
-    ('AMPLIFIER'),
-    ('DRUMS_AND_PERCUSSION'),
-    ('BASS_GUITAR'),
-    ('ACOUSTIC_GUITAR'),
-    ('SOFTWARE_AND_ACCESSORIES');
--- SQL/insertTestData.sql
+++ SQL/insertTestData.sql
@@ -1,3 +1,13 @@
+-- Добавление брендов
+INSERT INTO brand (name, country, website, email)
+SELECT 
+    'Brand ' || generate_series,
+    (SELECT enum_range(NULL::country_enum))[floor(random() * array_length(enum_range(NULL::country_enum), 1) + 1)],
+    'http://www.brand' || generate_series || '.com',
+    'contact@brand' || generate_series || '.com'
+FROM generate_series(1, 100);
+
+
 -- Добавление музыкантов
 INSERT INTO musician (name, subscribers)
 SELECT 
@@ -15,45 +25,87 @@ SELECT
 FROM generate_series(1, 13000);
 
 -- Добавление продуктов
-INSERT INTO product (name, description, price, rate, brand_id, guitar_form_id, type_of_product_id, 
-                    lads, color, strings, tip_material, body_material, pickup_configuration, type_combo_amplifier)
+INSERT INTO product (name, description, brand_id, guitar_form, type_of_product, 
+    lads, color, strings, tip_material, body_material, pickup_configuration, type_combo_amplifier)
 SELECT 
     'Product ' || generate_series,
     'Description for product ' || generate_series,
-    (random() * 10000)::numeric(10,2),
-    (random() * 4 + 1)::numeric(2,1),
     floor(random() * 10 + 1)::int,
-    floor(random() * 10 + 1)::int,
-    floor(random() * 9 + 1)::int,
+    (SELECT enum_range(NULL::guitar_form_enum))[floor(random() * array_length(enum_range(NULL::guitar_form_enum), 1) + 1)],
+    (SELECT enum_range(NULL::type_of_product_enum))[floor(random() * array_length(enum_range(NULL::type_of_product_enum), 1) + 1)],
     floor(random() * 24 + 1)::int,
-    (ARRAY['BLACK', 'WHITE', 'RED', 'BLUE', 'GREEN', 'YELLOW', 'PURPLE', 'ORANGE', 'PINK', 'BROWN', 'GREY', 'GOLD', 'SILVER'])[floor(random() * 13 + 1)],
+    (SELECT enum_range(NULL::color_enum))[floor(random() * array_length(enum_range(NULL::color_enum), 1) + 1)],
     floor(random() * 12 + 1)::int,
-    (ARRAY['Wood', 'Metal', 'Plastic', 'Composite'])[floor(random() * 4 + 1)],
-    (ARRAY['Mahogany', 'Maple', 'Ash', 'Alder', 'Basswood'])[floor(random() * 5 + 1)],
-    (ARRAY['SSS', 'HSS', 'HH', 'P90', 'Single'])[floor(random() * 5 + 1)],
-    (ARRAY['Tube', 'Solid State', 'Hybrid', 'Modeling'])[floor(random() * 4 + 1)]а
+    (SELECT enum_range(NULL::tip_material_enum))[floor(random() * array_length(enum_range(NULL::tip_material_enum), 1) + 1)],
+    (SELECT enum_range(NULL::body_material_enum))[floor(random() * array_length(enum_range(NULL::body_material_enum), 1) + 1)],
+    (SELECT enum_range(NULL::pickup_configuration_enum))[floor(random() * array_length(enum_range(NULL::pickup_configuration_enum), 1) + 1)],
+    (SELECT enum_range(NULL::type_combo_amplifier_enum))[floor(random() * array_length(enum_range(NULL::type_combo_amplifier_enum), 1) + 1)]
 FROM generate_series(1, 130000);
 
 -- Добавление статей
-INSERT INTO articles (header, text, author, date, tags, accepted)
+INSERT INTO articles (header, text, author_id, created_at, accepted)
 SELECT 
     'Article Header ' || generate_series,
     'Article Text ' || generate_series,
     floor(random() * 13000 + 1)::int,
     current_date - (random() * 365)::int,
-    'tag1, tag2, tag3',
     random() > 0.5
 FROM generate_series(1, 300);
 
 -- Добавление отзывов
-INSERT INTO feedback (user_id, product_id, article_id, text, stars)
+INSERT INTO feedback (author_id, product_id, article_id, text, stars, created_at)
 SELECT 
     floor(random() * 13000 + 1)::int,
     floor(random() * 130000 + 1)::int,
-    floor(random() * 200 + 1)::int,
+    floor(random() * 300 + 1)::int,
     'Feedback text ' || generate_series,
-    floor(random() * 5 + 1)::int
+    floor(random() * 5 + 1)::int,
+    current_date - (random() * 365)::int
 FROM generate_series(1, 5000);
+
+-- Добавление магазинов
+INSERT INTO shop (name, description, website, email, address)
+SELECT 
+    'Shop ' || generate_series,
+    'Description for shop ' || generate_series,
+    'http://shop' || generate_series || '.com',
+    'shop' || generate_series || '@example.com',
+    'Address ' || generate_series
+FROM generate_series(1, 15);
+
+-- Добавление продуктов в магазины
+INSERT INTO shop_product (shop_id, product_id, price, available)
+SELECT 
+    s.id,
+    p.id,
+    (random() * 10000)::numeric(10,2),
+    (ARRAY[TRUE, FALSE])[floor(random() * 2 + 1)]
+FROM product p
+CROSS JOIN LATERAL (
+    SELECT id
+    FROM shop
+    ORDER BY random()
+    LIMIT floor(random() * 4 + 1)
+) s;
+
+-- Добавление тем форума
+INSERT INTO forum_topic (title, description, author_id, is_closed)
+SELECT 
+    'Forum Topic ' || generate_series,
+    'Description for forum topic ' || generate_series,
+    floor(random() * 13000 + 1)::int,
+    (ARRAY[TRUE, FALSE])[floor(random() * 2 + 1)]
+FROM generate_series(1, 300);
+
+-- Добавление постов на форуме
+INSERT INTO forum_post (topic_id, content, author_id)
+SELECT 
+    floor(random() * 300 + 1)::int,
+    'Content for forum post ' || generate_series,
+    floor(random() * 13000 + 1)::int
+FROM generate_series(1, 1000);
+
+
 
 -- Добавление связей продуктов и статей
 INSERT INTO
@@ -114,15 +166,15 @@ FROM musician m
 
 -- Добавление связей жанров и пользователей
 INSERT INTO
-    genre_user (genre_id, user_id)
-SELECT g.id, u.id
+    genre_user (genre, user_id)
+SELECT g.genre, u.id
 FROM app_user u
     CROSS JOIN LATERAL (
-        SELECT id
-        FROM genre
+        SELECT genre
+        FROM (SELECT enum_range(NULL::genre_enum)) AS g(genre)
         WHERE
-            id NOT IN(
-                SELECT genre_id
+            genre NOT IN(
+                SELECT genre
                 FROM genre_user
                 WHERE
                     user_id = u.id
@@ -133,15 +185,15 @@ FROM app_user u
 
 -- Добавление связей типов музыкантов и пользователей
 INSERT INTO
-    type_of_musician_user (type_of_musician_id, user_id)
-SELECT t.id, u.id
+    type_of_musician_user (type_of_musician, user_id)
+SELECT t.type_of_musician, u.id
 FROM app_user u
     CROSS JOIN LATERAL (
-        SELECT id
-        FROM type_of_musician
+        SELECT type_of_musician
+        FROM (SELECT enum_range(NULL::type_of_musician_enum)) AS t(type_of_musician)
         WHERE
-            id NOT IN(
-                SELECT type_of_musician_id
+            type_of_musician NOT IN(
+                SELECT type_of_musician
                 FROM type_of_musician_user
                 WHERE
                     user_id = u.id
@@ -152,15 +204,15 @@ FROM app_user u
 
 -- Добавление связей продуктов и жанров
 INSERT INTO
-    product_genre (product_id, genre_id)
-SELECT p.id, g.id
+    product_genre (product_id, genre)
+SELECT p.id, g.genre
 FROM product p
     CROSS JOIN LATERAL (
-        SELECT id
-        FROM genre
+        SELECT genre
+        FROM (SELECT enum_range(NULL::genre_enum)) AS g(genre)
         WHERE
-            id NOT IN(
-                SELECT genre_id
+            genre NOT IN(
+                SELECT genre
                 FROM product_genre
                 WHERE
                     product_id = p.id
@@ -171,15 +223,15 @@ FROM product p
 
 -- Добавление связей музыкантов и жанров
 INSERT INTO
-    musician_genre (musician_id, genre_id)
-SELECT m.id, g.id
+    musician_genre (musician_id, genre)
+SELECT m.id, g.genre
 FROM musician m
     CROSS JOIN LATERAL (
-        SELECT id
-        FROM genre
+        SELECT genre
+        FROM (SELECT enum_range(NULL::genre_enum)) AS g(genre)
         WHERE
-            id NOT IN(
-                SELECT genre_id
+            genre NOT IN(
+                SELECT genre
                 FROM musician_genre
                 WHERE
                     musician_id = m.id
@@ -210,17 +262,17 @@ FROM app_user u
 -- Добавление связей типов музыкантов и музыкантов
 INSERT INTO
     type_of_musician_musician (
-        type_of_musician_id,
+        type_of_musician,
         musician_id
     )
-SELECT t.id, m.id
+SELECT t.type_of_musician, m.id
 FROM musician m
     CROSS JOIN LATERAL (
-        SELECT id
-        FROM type_of_musician
+        SELECT type_of_musician
+        FROM (SELECT enum_range(NULL::type_of_musician_enum)) AS t(type_of_musician)
         WHERE
-            id NOT IN(
-                SELECT type_of_musician_id
+            type_of_musician NOT IN(
+                SELECT type_of_musician
                 FROM type_of_musician_musician
                 WHERE
                     musician_id = m.id
