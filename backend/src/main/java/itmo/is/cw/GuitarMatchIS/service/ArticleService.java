f6ad4de80a239c04d847a7d08cfcf5c160c2a614
ta4ilka69 <artem_balin_2004@mail.ru>
2024-12-21 00:10:00 +0300

    unused import

--- backend/src/main/java/itmo/is/cw/GuitarMatchIS/service/ArticleService.java
+++ backend/src/main/java/itmo/is/cw/GuitarMatchIS/service/ArticleService.java
@@ -3,8 +3,6 @@ package itmo.is.cw.GuitarMatchIS.service;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
