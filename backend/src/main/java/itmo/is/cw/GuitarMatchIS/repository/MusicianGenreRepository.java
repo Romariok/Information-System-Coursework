eeae7980db634c3513110339db88042d1cedae0c
Romariok <ronya-ko@yandex.ru>
2024-12-20 23:50:13 +0300

    change musician creation - add Genre and type of musician lists

--- backend/src/main/java/itmo/is/cw/GuitarMatchIS/repository/MusicianGenreRepository.java
+++ backend/src/main/java/itmo/is/cw/GuitarMatchIS/repository/MusicianGenreRepository.java
@@ -5,9 +5,18 @@ import java.util.List;
 import itmo.is.cw.GuitarMatchIS.models.MusicianGenre;
 import itmo.is.cw.GuitarMatchIS.models.Musician;
 import itmo.is.cw.GuitarMatchIS.models.keys.MusicianGenreId;
+import jakarta.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MusicianGenreRepository extends JpaRepository<MusicianGenre, MusicianGenreId> {
    List<MusicianGenre> findByMusician(Musician musician);
+
+   @Transactional
+   @Modifying
+   @Query(value = "INSERT INTO musician_genre (musician_id, genre) VALUES (:musicianId, CAST(:genre AS genre_enum))", nativeQuery = true)
+   void saveByMusicianIdAndGenre(@Param("musicianId") Long musicianId, @Param("genre") String genre);
 }
