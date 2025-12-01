4a6ef5e5e5b52d2600f14f8b79c5f9c3dc600a59
ta4ilka69 <artem_balin_2004@mail.ru>
2025-12-02 00:07:19 +0300

    article optimization

--- backend/src/main/java/itmo/is/cw/GuitarMatchIS/repository/ArticleRepository.java
+++ backend/src/main/java/itmo/is/cw/GuitarMatchIS/repository/ArticleRepository.java
@@ -1,14 +1,15 @@
 package itmo.is.cw.GuitarMatchIS.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import itmo.is.cw.GuitarMatchIS.models.Article;
 
 import java.util.List;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
@@ -17,12 +18,16 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
    @Query("SELECT moderate_article(:articleId, :accepted, :moderatorId)")
    boolean moderateArticle(Long articleId, boolean accepted, Long moderatorId);
 
+   @EntityGraph(attributePaths = "author")
    Page<Article> findByHeaderContainingAndAccepted(String header, boolean accepted, Pageable page);
 
+   @EntityGraph(attributePaths = "author")
    Page<Article> findByAuthorIdAndAccepted(Long authorId, boolean accepted, Pageable page);
 
+   @EntityGraph(attributePaths = "author")
    Page<Article> findByAccepted(boolean accepted, Pageable page);
 
+   @EntityGraph(attributePaths = "author")
    List<Article> findByAccepted(boolean accepted);
 
 }
