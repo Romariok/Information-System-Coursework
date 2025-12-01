fbbac4f154e9a287db5ba1bd482bfe8376e96a88
Romariok <ronya-ko@yandex.ru>
2025-12-01 21:11:47 +0300

    added redis to optimize requests

--- backend/src/main/java/itmo/is/cw/GuitarMatchIS/GuitarMatchISApplication.java
+++ backend/src/main/java/itmo/is/cw/GuitarMatchIS/GuitarMatchISApplication.java
@@ -2,9 +2,11 @@ package itmo.is.cw.GuitarMatchIS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableCaching
 @EnableAsync
 public class GuitarMatchISApplication {
 
