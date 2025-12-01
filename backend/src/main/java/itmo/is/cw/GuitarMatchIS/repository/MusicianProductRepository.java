8c83af65cf2b176c3b7eefef5d5cb77ab6879796
ta4ilka69 <artem_balin_2004@mail.ru>
2025-12-01 23:33:13 +0300

    musician optimization

--- backend/src/main/java/itmo/is/cw/GuitarMatchIS/repository/MusicianProductRepository.java
+++ backend/src/main/java/itmo/is/cw/GuitarMatchIS/repository/MusicianProductRepository.java
@@ -2,17 +2,19 @@ package itmo.is.cw.GuitarMatchIS.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import itmo.is.cw.GuitarMatchIS.models.Musician;
 import itmo.is.cw.GuitarMatchIS.models.MusicianProduct;
 import itmo.is.cw.GuitarMatchIS.models.Product;
 import itmo.is.cw.GuitarMatchIS.models.keys.MusicianProductId;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 public interface MusicianProductRepository extends JpaRepository<MusicianProduct, MusicianProductId> {
    List<MusicianProduct> findByMusician(Musician musician);
+
+   List<MusicianProduct> findByMusicianIdIn(List<Long> musicianIds);
 
    Page<MusicianProduct> findByProduct(Product product, Pageable pageable);
 
