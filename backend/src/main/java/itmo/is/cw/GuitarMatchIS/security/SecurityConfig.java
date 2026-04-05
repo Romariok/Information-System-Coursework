82c154bfd809b570a3e7d06d1e2f00490716dc0e
Romariok <ronya-ko@yandex.ru>
2026-04-05 22:45:28 +0300

    added docs files

--- backend/src/main/java/itmo/is/cw/GuitarMatchIS/security/SecurityConfig.java
+++ backend/src/main/java/itmo/is/cw/GuitarMatchIS/security/SecurityConfig.java
@@ -57,6 +57,7 @@ public class SecurityConfig {
                         .requestMatchers("/index.html", "/favicon.ico", "/static/**").permitAll()
                         .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers("/api/auth/**", "/api/user/role/**", "/ws", "/api/").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/admin/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.POST, "/api/moderate/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
